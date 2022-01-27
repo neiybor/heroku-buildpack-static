5a9b6181866dabef76197786d58784d0111e56ff
haotsai101 <tsaizhihao@gmail.com>
2022-01-27 10:07:50 -0700

    save progress

--- scripts/config/lib/ngx_mruby/listing_redirect_path.rb
+++ scripts/config/lib/ngx_mruby/listing_redirect_path.rb
@@ -9,5 +9,6 @@ req         = Nginx::Request.new
 uri         = req.var.uri
 
 city, state, id = uri.match(%r{/listings/([A-z0-9\-]*)\/([A-z0-9\-]*)\/([0-9]*)$}mi).captures
+puts "listings/#{state.downcase}/#{city.downcase}/#{id.downcase}"
 
-"storage-units-near-me/#{state.downcase}/#{city.downcase}/#{id.downcase}"
+"listings/#{state.downcase}/#{city.downcase}/#{id.downcase}"
